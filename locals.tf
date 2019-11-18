d68725bf1adc8fc47d75e8f75729cc59737fe4fb
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-11-18 18:16:07 +0100

    AZ-129: Replace unallowed chars

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
-  name_prefix = "${var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""}"
-  acr_name    = "${local.name_prefix}${var.stack}${var.client_name}${var.location_short}${var.environment}acr"
+  name_prefix      = "${var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""}"
+  acr_default_name = "${local.name_prefix}${var.stack}${var.client_name}${var.location_short}${var.environment}acr"
+  acr_name         = "${replace(coalesce(var.custom_name, local.acr_name), "-_", "")}"
 
   default_tags = {
     env   = "${var.environment}"
--- resources.tf
+++ resources.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "registry" {
-  name = "${coalesce(var.custom_name, local.acr_name)}"
+  name = "${local.acr_name}"
 
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
