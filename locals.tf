71ab0ff96de3abf01147ff6b7c91c4e3e521086c
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-11-20 11:02:00 +0100

    AZ-129: No need to suffix with dash the prefix var

--- locals.tf
+++ locals.tf
@@ -1,6 +1,5 @@
 locals {
-  name_prefix           = "${var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""}"
-  acr_default_name_long = "${local.name_prefix}${var.stack}${var.client_name}${var.location_short}${var.environment}acr"
+  acr_default_name_long = "${var.name_prefix}${var.stack}${var.client_name}${var.location_short}${var.environment}acr"
   acr_name              = "${coalesce(var.custom_name, substr(local.acr_default_name_long, 0, length(local.acr_default_name_long) > 50 ? 49 : -1))}"
 
   default_tags = {
