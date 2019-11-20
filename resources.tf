7ee1d1982ae68676844e57251a375bc7c0303704
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-11-20 10:44:25 +0100

    AZ-129: TF 0.11 hack with list

--- resources.tf
+++ resources.tf
@@ -4,9 +4,11 @@ resource "azurerm_container_registry" "registry" {
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
 
-  sku                      = "${var.sku}"
-  admin_enabled            = "${var.admin_enabled}"
-  georeplication_locations = ["${var.georeplication_locations}"]
+  sku           = "${var.sku}"
+  admin_enabled = "${var.admin_enabled}"
+
+  # TF 0.11 hack to remove with HCL2
+  georeplication_locations = ["${coalescelist(var.georeplication_locations, list(var.location))}"]
 
   tags = "${merge(local.default_tags, var.extra_tags)}"
 }
