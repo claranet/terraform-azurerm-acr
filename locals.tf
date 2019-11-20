e77a49d375410c1d44d0c7b9ec2fa07af245e4ba
Jérôme Respaut <jerome.respaut@fr.clara.net>
2019-11-20 11:07:41 +0100

    AZ-129: Replace not allowed chars in name

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   acr_default_name_long = "${var.name_prefix}${var.stack}${var.client_name}${var.location_short}${var.environment}acr"
-  acr_name              = "${coalesce(var.custom_name, substr(local.acr_default_name_long, 0, length(local.acr_default_name_long) > 50 ? 49 : -1))}"
+  acr_name              = "${coalesce(var.custom_name, substr(replace(local.acr_default_name_long, "/[-_]/", ""), 0, length(local.acr_default_name_long) > 50 ? 49 : -1))}"
 
   default_tags = {
     env   = "${var.environment}"
