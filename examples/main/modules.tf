d9ada204466d2d26ffe1ffb65b28eb8cbf2257b0
Rémi Beaufils <remi.beaufils@fr.clara.net>
2021-09-20 09:11:10 +0200

    AZ-572: Revamp examples + improve CI

---
+++ examples/main/modules.tf
@@ -0,0 +1,30 @@
+module "azure_region" {
+  source  = "claranet/regions/azurerm"
+  version = "x.x.x"
+
+  azure_region = var.azure_region
+}
+
+module "rg" {
+  source  = "claranet/rg/azurerm"
+  version = "x.x.x"
+
+  location    = module.azure_region.location
+  client_name = var.client_name
+  environment = var.environment
+  stack       = var.stack
+}
+
+module "acr" {
+  source  = "claranet/acr/azurerm"
+  version = "x.x.x"
+
+  location            = module.azure_region.location
+  location_short      = module.azure_region.location_short
+  resource_group_name = module.rg.resource_group_name
+  sku                 = "Standard"
+
+  client_name = var.client_name
+  environment = var.environment
+  stack       = var.stack
+}
