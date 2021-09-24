a1ca478ad2effa97ccf586dd987d71962740f130
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2021-09-24 13:35:43 +0200

    AZ-504: Fix CI

--- examples/main/modules.tf
+++ examples/main/modules.tf
@@ -15,16 +15,36 @@ module "rg" {
   stack       = var.stack
 }
 
+module "logs" {
+  source  = "claranet/run-common/azurerm//modules/logs"
+  version = "x.x.x"
+
+  client_name         = var.client_name
+  environment         = var.environment
+  stack               = var.stack
+  location            = module.azure_region.location
+  location_short      = module.azure_region.location_short
+  resource_group_name = module.rg.resource_group_name
+}
+
 module "acr" {
   source  = "claranet/acr/azurerm"
   version = "x.x.x"
 
+  client_name         = var.client_name
+  environment         = var.environment
+  stack               = var.stack
   location            = module.azure_region.location
   location_short      = module.azure_region.location_short
   resource_group_name = module.rg.resource_group_name
   sku                 = "Standard"
 
-  client_name = var.client_name
-  environment = var.environment
-  stack       = var.stack
+  logs_destinations_ids = [
+    module.logs.logs_storage_account_id,
+    module.logs.log_analytics_workspace_id
+  ]
+
+  extra_tags = {
+    foo = "bar"
+  }
 }
