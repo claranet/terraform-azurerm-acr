43cb8950725a25ed5c4d29835aaaeea46db4a649
F PERAUD <frederic.peraud@fr.clara.net>
2021-09-30 13:54:14 +0200

    AZ-590: rename georeplication_locations (deprecated) parameter to georeplication

--- resources.tf
+++ resources.tf
@@ -1,11 +1,11 @@
 resource "azurerm_container_registry" "registry" {
   name = local.acr_name
 
-  location                 = var.location
-  resource_group_name      = var.resource_group_name
-  sku                      = var.sku
-  admin_enabled            = var.admin_enabled
-  georeplication_locations = var.georeplication_locations
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  admin_enabled       = var.admin_enabled
+  georeplication      = var.georeplication_locations
 
   retention_policy {
     enabled = var.images_retention_enabled
